ab03e74dac777234bc7b3e599a816d0f4a50657a
jdmoody <moodyofjonathan@gmail.com>
2014-04-12 23:27:52 -0700

    Improved memory efficiency

--- all_sums.rb
+++ all_sums.rb
@@ -1,28 +1,23 @@
 def all_sums(num)
   return [num.to_i] if num.length < 2
+  
   sums = []
   nums_arr = num.split("")
   max_pluses = nums_arr.length - 1
   plus_combinations = 2 ** max_pluses
   
-  (0..plus_combinations - 1).each do |pluses|
-    nums_permutation = nums_arr.dup
-    idx = 0
-    binary = pluses.to_s(2).split('')
+  (0..plus_combinations - 1).each do |pluses_combo|
+    nums_to_sum = [nums_arr[0].to_i]
     
-    until binary.length == max_pluses
-      binary.unshift("0")
-    end
-    
-    binary.each do |plus|
-      if plus == "0"
-        nums_permutation[idx] = nums_permutation[idx..(idx + 1)].join
-        nums_permutation.slice!(idx + 1)
+    (max_pluses - 1).downto(0) do |bit|
+      if pluses_combo[bit] == 0
+        nums_to_sum[-1] = nums_to_sum.last * 10 + nums_arr[-bit - 1].to_i
       else
-        idx += 1
+        nums_to_sum << nums_arr[-bit - 1].to_i
       end
     end
-    sums << nums_permutation.map(&:to_i).inject(&:+)
+    
+    sums << nums_to_sum.inject(&:+)
   end
   sums
 end
